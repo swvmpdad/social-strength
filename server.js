fa2fb9effb34bd9eb677a530a52613e0b54e0eb9
meza68k <meza24k@gmail.com>
2022-08-31 12:01:36 -0500

    routes sortve work

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 const path = require('path');
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    // store: new SequelizeStore({
+    //     db: sequelize,
+    // }),
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
--- utils/auth.js
+++ utils/auth.js
@@ -1,16 +1,12 @@
 const withAuth = (req, res, next) => {
-  // TODO: If the user is not logged in, redirect the user to the login page
-  
-  router.get('/login', (req, res) => {
+  // If the user is not logged in, redirect the request to the login route
     // If the user is already logged in, redirect to the homepage
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-    // Otherwise, render the 'login' template
-    res.render('login');
-  });
-  // TODO: If the user is logged in, allow them to view the paintings
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+    next();  
+  };
 };
+  
 
 module.exports = withAuth;
