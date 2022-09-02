de90684274b79425bede69458bc46ead566a931b
meza68k <meza24k@gmail.com>
2022-09-01 19:43:46 -0500

    signup and logout works

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: 'Super secret secret',
-    resave: true,
+    resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize,
