84f8eb32c6354ceb98b694e60f67b5a56d8d982d
swvmpdad <will.g.camp@gmail.com>
2022-09-06 19:16:16 -0500

    removed routine-exercises-routes from the api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,14 +6,10 @@ const exerciseRoutes = require('./exercise-routes');
 
 const routineRoutes = require('./routine-routes');
 
-const routineExerciseRoutes = require('./routine-exercises-routes')
-
 router.use('/users', userRoutes);
 
 router.use('/exercises', exerciseRoutes);
 
 router.use('/routines', routineRoutes);
-
-router.use('/routineexercises', routineExerciseRoutes);
 
 module.exports = router;
